ce8eea1d23a2df81e80fc4e7d4867b3a3627a45d
gowthamvetri <gowthamvetriii@gmail.com>
2026-05-27 18:01:56 +0530

    logs storage got removed - fix

--- MLA-Backend/src/shared/logger/index.ts
+++ MLA-Backend/src/shared/logger/index.ts
@@ -23,17 +23,6 @@ const logger = createLogger({
     new transports.Console({
       format: format.combine(format.colorize(), logFormat),
     }),
-    new transports.File({
-      filename: path.join(__dirname, '../../logs/error.log'),
-      level: 'error',
-      maxsize: 5242880, // 5MB
-      maxFiles: 3,
-    }),
-    new transports.File({
-      filename: path.join(__dirname, '../../logs/combined.log'),
-      maxsize: 5242880,
-      maxFiles: 3,
-    }),
   ],
   exitOnError: false,
 });
